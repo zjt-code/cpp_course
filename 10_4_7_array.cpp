a26f3e9ecb511a0854bdbf8515c47352d6dda737
zjt-code <zhangjingtao500@126.com>
2024-07-30 10:17:16 +0800

    add new section
    1, add section 10.4.7 array
    2, add section 10.4.8 local static variable

--- 10_4_7_array.cpp
+++ 10_4_7_array.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-
+#include<vector>
 
 
 class Table
@@ -19,11 +19,42 @@ public:
 };
 int Table::con_cnt =0;
 
+
+void fn(int sz)
+{
+    Table *t1  = new Table;
+    Table *t2  = new Table[sz];
+    Table *t3 =  new Table;
+    Table *t4 =  new Table[sz];
+
+    delete t1;
+    delete[] t2;
+    delete t3;
+    delete[] t4;
+
+}
+
+
+void g()
+{
+    std::vector<Table> *p1 = new std::vector<Table>(10);
+
+    Table * p2 = new Table;
+
+
+    delete p1;
+    delete p2;
+
+}
+
 int main(int argn, char *argv[])
 {
 
 
     Table tab[10];
 
+    fn(10);
+
+    std::cout<<"hello worldl lll"<<std::endl;
     return 0;
 }
---
+++ 10_4_8_localStaticVar.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+
+class Table
+{
+    private:
+     int v;
+    public:
+    Table(int x ):v(x)
+    {
+        std::cout<<"construct = "<< x << std::endl;
+    };
+
+    ~Table()
+    {
+        std::cout<<"disconstruct  v ="<< v << std::endl;
+    };
+};
+
+
+//
+
+void fn(int x)
+{
+
+    static Table t1(1);
+
+    if(x==1)
+    {
+        static Table t2(2);
+    }
+    else
+    {
+
+        static Table t3(3);
+    }
+
+
+}
+
+int main(int argn, char *agrv[])
+{
+
+    fn(1);
+
+
+
+    return 0;
+}
